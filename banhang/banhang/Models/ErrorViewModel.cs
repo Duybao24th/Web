923c9439afb10ee05b88c0188320c5b60bd66ca9
Duybao24th <baond.24th@sv.dla.edu.vn>
2026-07-19 17:02:54 +0700

    Delete banhang directory

--- banhang/banhang/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace banhang.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
